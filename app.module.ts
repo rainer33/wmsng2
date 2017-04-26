910aeab19fed10de0acb12958b8d1b58a78a3a45
Jahong Min <rainer33@naver.com>
2017-04-26 18:04:12 +0900

    ng2
    구조를 알고 싶다

---
+++ app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { ManagerSharedModule, UserRouteAccessService } from './shared';
+import { ManagerHomeModule } from './home/home.module';
+import { ManagerAdminModule } from './admin/admin.module';
+import { ManagerAccountModule } from './account/account.module';
+import { ManagerEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        ManagerSharedModule,
+        ManagerHomeModule,
+        ManagerAdminModule,
+        ManagerAccountModule,
+        ManagerEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class ManagerAppModule {}
